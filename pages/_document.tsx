110343d99a7237d69a9a82cefdffe7815782538f
Jessie <740395858@qq.com>
2025-01-15 17:37:47 +0800

    fix: 国际化

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,23 @@ export default function Document() {
           name="google-site-verification"
           content="W2wkuqoDDz3O3Q48KLqG2HQOSnx4gFvUU8yLgOcXT-0"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/avatar.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/avatar.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/avatar.png"
+        />
       </Head>
       <body>
         <noscript>
